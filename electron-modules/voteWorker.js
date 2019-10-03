6bdd5dcd019e6636f083c3557b1b1f68952aaeb3
cybor97 <muhametshin.1997@gmail.com>
2019-10-04 02:33:14 +0300

    Added strict vote once per day

--- electron-modules/voteWorker.js
+++ electron-modules/voteWorker.js
@@ -4,6 +4,7 @@ const { IconWallet, IconBuilder, HttpProvider, SignedTransaction } = IconService
 
 //Day interval
 const UPDATE_INTERVAL = 86400000;
+const CHECK_INTERVAL = 300000;
 //5 seconds
 const TX_DELAY = 5000;
 const API_URL = 'https://ctz.solidwallet.io/api/v3';
@@ -24,124 +25,141 @@ async function start(myAddress, privateKey, eventSender) {
     }
 
     while (true) {
-        let stakes = await db.findAsync({ model: 'Stake' });
-
-        if (stakes.length) {
-            try {
-                //STEP 1: SHOULD BE LARGER THAN 1
-                let claimableICX = await iconService.call(new IconBuilder.CallBuilder()
-                    .to('cx0000000000000000000000000000000000000000')
-                    .method('queryIScore')
-                    .params({ address: myAddress })
-                    .build()
-                ).execute();
-                eventSender.send('/logs', `claimableICX: ${parseInt(claimableICX.estimatedICX)}`);
-
-                if (parseInt(claimableICX.estimatedICX) >= 1) {
-                    //STEP 2: CLAIM AVAILABLE ISCORE
-                    let claimIScoreResult = await iconService.sendTransaction(
-                        new SignedTransaction(new IconBuilder.CallTransactionBuilder()
-                            .from(myAddress)
-                            .to('cx0000000000000000000000000000000000000000')
-                            .version('0x3')
-                            .nid('0x1')
-                            .nonce('0x0')
-                            .value('0x0')
-                            .stepLimit(IconService.IconConverter.toBigNumber(108000))
-                            .timestamp(Date.now() * 1000)
-                            .method('claimIScore')
-                            .build(), iconWallet)
-                    ).execute();
-                    eventSender.send('/logs', `claimIScoreResult: ${claimIScoreResult}`);
-                    await new Promise(resolve => setTimeout(resolve, TX_DELAY));
-                }
-
-                //STEP 3: GET AVAILABLE BALANCE FOR STAKING
-                let balance = await iconService.getBalance(myAddress).execute();
-                eventSender.send('/logs', `balance: ${balance.toNumber()}`);
-
-                //STEP 4: GET ALREADY STAKED BALANCE
-                let stakedBalance = await iconService.call(new IconBuilder.CallBuilder()
-                    .to('cx0000000000000000000000000000000000000000')
-                    .method('getStake')
-                    .params({ address: myAddress })
-                    .build()
-                ).execute();
-                eventSender.send('/logs', `stakedBalance: ${parseInt(stakedBalance.stake)}`);
-
-                let valueToStake = balance.toNumber() - ICX_PRESERVE + parseInt(stakedBalance.stake);
-                eventSender.send('/logs', `valueToStake: ${valueToStake}`);
-
-                if (valueToStake > ICX_PRESERVE) {
-                    //STEP 5: STAKE ALL AVAILABLE BALANCE
-                    let setStakeResult = await iconService.sendTransaction(
-                        new SignedTransaction(new IconBuilder.CallTransactionBuilder()
-                            .from(myAddress)
-                            .to('cx0000000000000000000000000000000000000000')
-                            .version('0x3')
-                            .nid('0x1')
-                            .nonce('0x0')
-                            .value('0x0')
-                            .method('setStake')
-                            //TODO: Review
-                            .stepLimit(IconService.IconConverter.toBigNumber(125000))
-                            .timestamp(Date.now() * 1000)
-                            .params({ value: `0x${valueToStake.toString(16)}` })
-                            .build(), iconWallet)
-                    ).execute();
-                    eventSender.send('/logs', `setStakeResult: ${setStakeResult}`);
-
-                    await new Promise(resolve => setTimeout(resolve, TX_DELAY));
-                }
-
-                //STEP 6: CHECK VOTING POWER
-                let votingPowerTotal = await iconService.call(new IconBuilder.CallBuilder()
-                    .to('cx0000000000000000000000000000000000000000')
-                    .method('getStake')
-                    .params({ address: myAddress })
-                    .build()
-                ).execute();
-                eventSender.send('/logs', `votingPower: ${parseInt(votingPowerTotal.stake)}`);
-
-                let stakesValueSum = stakes.reduce((prev, next) => prev + next.value, 0);
-                eventSender.send('/logs', `stakesValueSum: ${stakesValueSum}`);
-
-                if (stakesValueSum > 100) {
-                    throw new ValidationError(`Stakes value sum too large(${stakesValueSum} > 100)`);
-                }
-
-                //STEP 7: VOTE FOR ADDRESS
-                let setDelegationResult = await iconService.sendTransaction(
-                    new SignedTransaction(new IconBuilder.CallTransactionBuilder()
-                        .from(myAddress)
-                        .to('cx0000000000000000000000000000000000000000')
-                        .version('0x3')
-                        .nid('0x1')
-                        .nonce('0x0')
-                        .value('0x0')
-                        .stepLimit(IconService.IconConverter.toBigNumber(125000 + 25000 * stakes.length))
-                        .timestamp(Date.now() * 1000)
-                        .method('setDelegation')
-                        .params({
-                            delegations: stakes.map(stake => ({
-                                address: stake.address,
-                                value: `0x${((stake.value / 100) * votingPowerTotal.stake).toString(16)}`
-                            }))
-                        })
-                        .build(), iconWallet)
-                ).execute();
-                eventSender.send('/logs', `setDelegation result: ${setDelegationResult}`);
-            }
-            catch (err) {
-                console.error(err);
-                eventSender.send('/error', err);
-            }
+        let lastUpdate = await db.findAsync({ model: 'LastUpdate' });
+        let shouldUpdate = false;
+        if (!lastUpdate.length) {
+            await db.insertAsync({ model: 'LastUpdate', time: Date.now() });
+            shouldUpdate = true;
         }
         else {
-            eventSender.send('/logs', 'Skipped, invalid amount of votes');
+            if (lastUpdate < Date.now() - UPDATE_INTERVAL) {
+                shouldUpdate = true;
+            }
         }
 
-        await new Promise(resolve => setTimeout(resolve, UPDATE_INTERVAL));
+        if (shouldUpdate) {
+            eventSender.send('/logs', `Should update`);
+
+            let stakes = await db.findAsync({ model: 'Stake' });
+
+            if (stakes.length) {
+                try {
+                    //STEP 1: SHOULD BE LARGER THAN 1
+                    let claimableICX = await iconService.call(new IconBuilder.CallBuilder()
+                        .to('cx0000000000000000000000000000000000000000')
+                        .method('queryIScore')
+                        .params({ address: myAddress })
+                        .build()
+                    ).execute();
+                    eventSender.send('/logs', `claimableICX: ${parseInt(claimableICX.estimatedICX)}`);
+
+                    if (parseInt(claimableICX.estimatedICX) >= 1) {
+                        //STEP 2: CLAIM AVAILABLE ISCORE
+                        let claimIScoreResult = await iconService.sendTransaction(
+                            new SignedTransaction(new IconBuilder.CallTransactionBuilder()
+                                .from(myAddress)
+                                .to('cx0000000000000000000000000000000000000000')
+                                .version('0x3')
+                                .nid('0x1')
+                                .nonce('0x0')
+                                .value('0x0')
+                                .stepLimit(IconService.IconConverter.toBigNumber(108000))
+                                .timestamp(Date.now() * 1000)
+                                .method('claimIScore')
+                                .build(), iconWallet)
+                        ).execute();
+                        eventSender.send('/logs', `claimIScoreResult: ${claimIScoreResult}`);
+                        await new Promise(resolve => setTimeout(resolve, TX_DELAY));
+                    }
+
+                    //STEP 3: GET AVAILABLE BALANCE FOR STAKING
+                    let balance = await iconService.getBalance(myAddress).execute();
+                    eventSender.send('/logs', `balance: ${balance.toNumber()}`);
+
+                    //STEP 4: GET ALREADY STAKED BALANCE
+                    let stakedBalance = await iconService.call(new IconBuilder.CallBuilder()
+                        .to('cx0000000000000000000000000000000000000000')
+                        .method('getStake')
+                        .params({ address: myAddress })
+                        .build()
+                    ).execute();
+                    eventSender.send('/logs', `stakedBalance: ${parseInt(stakedBalance.stake)}`);
+
+                    let valueToStake = balance.toNumber() - ICX_PRESERVE + parseInt(stakedBalance.stake);
+                    eventSender.send('/logs', `valueToStake: ${valueToStake}`);
+
+                    if (valueToStake > ICX_PRESERVE) {
+                        //STEP 5: STAKE ALL AVAILABLE BALANCE
+                        let setStakeResult = await iconService.sendTransaction(
+                            new SignedTransaction(new IconBuilder.CallTransactionBuilder()
+                                .from(myAddress)
+                                .to('cx0000000000000000000000000000000000000000')
+                                .version('0x3')
+                                .nid('0x1')
+                                .nonce('0x0')
+                                .value('0x0')
+                                .method('setStake')
+                                //TODO: Review
+                                .stepLimit(IconService.IconConverter.toBigNumber(125000))
+                                .timestamp(Date.now() * 1000)
+                                .params({ value: `0x${valueToStake.toString(16)}` })
+                                .build(), iconWallet)
+                        ).execute();
+                        eventSender.send('/logs', `setStakeResult: ${setStakeResult}`);
+
+                        await new Promise(resolve => setTimeout(resolve, TX_DELAY));
+                    }
+
+                    //STEP 6: CHECK VOTING POWER
+                    let votingPowerTotal = await iconService.call(new IconBuilder.CallBuilder()
+                        .to('cx0000000000000000000000000000000000000000')
+                        .method('getStake')
+                        .params({ address: myAddress })
+                        .build()
+                    ).execute();
+                    eventSender.send('/logs', `votingPower: ${parseInt(votingPowerTotal.stake)}`);
+
+                    let stakesValueSum = stakes.reduce((prev, next) => prev + next.value, 0);
+                    eventSender.send('/logs', `stakesValueSum: ${stakesValueSum}`);
+
+                    if (stakesValueSum > 100) {
+                        throw new ValidationError(`Stakes value sum too large(${stakesValueSum} > 100)`);
+                    }
+
+                    //STEP 7: VOTE FOR ADDRESS
+                    let setDelegationResult = await iconService.sendTransaction(
+                        new SignedTransaction(new IconBuilder.CallTransactionBuilder()
+                            .from(myAddress)
+                            .to('cx0000000000000000000000000000000000000000')
+                            .version('0x3')
+                            .nid('0x1')
+                            .nonce('0x0')
+                            .value('0x0')
+                            .stepLimit(IconService.IconConverter.toBigNumber(125000 + 25000 * stakes.length))
+                            .timestamp(Date.now() * 1000)
+                            .method('setDelegation')
+                            .params({
+                                delegations: stakes.map(stake => ({
+                                    address: stake.address,
+                                    value: `0x${((stake.value / 100) * votingPowerTotal.stake).toString(16)}`
+                                }))
+                            })
+                            .build(), iconWallet)
+                    ).execute();
+                    eventSender.send('/logs', `setDelegation result: ${setDelegationResult}`);
+                }
+                catch (err) {
+                    console.error(err);
+                    eventSender.send('/error', err);
+                }
+            }
+            else {
+                eventSender.send('/logs', 'Skipped, invalid amount of votes');
+            }
+            await db.updateAsync({ model: 'LastUpdate' }, { model: 'LastUpdate', time: Date.now() }, { upsert: true });
+        }
+
+        await new Promise(resolve => setTimeout(resolve, CHECK_INTERVAL));
     }
 }
 
