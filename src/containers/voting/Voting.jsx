38b90bbe8343ee71ab285d47e009dec09bed5754
cybor97 <muhametshin.1997@gmail.com>
2019-10-04 02:19:01 +0300

    Promo block should not hide

--- src/containers/voting/Voting.jsx
+++ src/containers/voting/Voting.jsx
@@ -20,7 +20,7 @@ export class Voting extends Component {
             stakes: {},
             logs: [],
             tabIndex: 1,
-            showPromo: !localStorage.getItem('promoAccepted')
+            // showPromo: !localStorage.getItem('promoAccepted')
         };
     }
 
@@ -113,7 +113,7 @@ export class Voting extends Component {
                     {self.state.loading && <Loading />}
                 </Tabs>
             </div>
-            {self.state.showPromo && <div className="promo-container">
+            <div className="promo-container">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 18C5.58 18 2 14.42 2 10C2 5.58 5.58 2 10 2C14.42 2 18 5.58 18 10C18 14.42 14.42 18 10 18ZM10 15C12.7614 15 15 12.7614 15 10C15 7.23858 12.7614 5 10 5C7.23858 5 5 7.23858 5 10C5 12.7614 7.23858 15 10 15Z" fill="#9E9E9E" />
                 </svg>
@@ -125,7 +125,7 @@ export class Voting extends Component {
                     self.setState({ showPromo: false, stakes: stakes });
                     localStorage.setItem('promoAccepted', true);
                 }}>RUN</button>
-            </div>}
+            </div>
         </div>
     }
 }
